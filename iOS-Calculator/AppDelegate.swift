ad1949ec377b375c773d843bc7fca0783542163b
Sebastian Rozo <sebastian.rozo@pragma.com.co>
2021-06-22 20:43:23 -0500

    se agrega nueva interfaz de cualculadora (view)

--- iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/AppDelegate.swift
@@ -10,25 +10,25 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow? // contenerdor de app, conjunto de app
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // setup
+        setupView()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+ 
+    // MARK : - private methods
+    
+    private func setupView(){
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc //el controlador de vista principal es ...
+        window?.makeKeyAndVisible() // se inicie y se muestre visible
+        
     }
 
 
---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Sebastian Rozo on 22/06/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - Life Cycle
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
